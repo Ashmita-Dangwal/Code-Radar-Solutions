440720d182381ad7c62af46e6f0f2b9f77d82ec6
Ashmita-Dangwal <ashmitadangwal43@gmail.com>
2025-01-20 21:53:48 +0530

    Solution to Check Character Type

---
+++ Check Character Type@@6788dd63490637cfc44e1b95/code.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main()
+{
+    char c;
+    scanf("%c",&c);
+    if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u')
+    {
+        printf("Vowel");
+    }
+    else if(c=='b'||c=='c'||c=='d'||c=='f'||c=='g'||c=='h'||c=='j'||c=='k'||c=='l'||c=='m'||c=='n'||c=='p'||c=='q'||c=='r'||c=='s'||c=='t'||c=='v'||c=='w'||c=='x'||c=='y'||c=='z')
+    {
+        printf("Consonant");
+    }
+    else if(c=='0'||c=='1'||c=='2'||c=='3'||c=='4'||c=='5'||c=='6'||c=='7'||c=='8'||c=='9')
+    {
+        printf("Digit");
+    }
+    else
+    {
+        printf("Special Character");
+    }
+    return 0;
+
+}
