4b87d9a615e160929bc31fae72879903d857d783
Ashmita-Dangwal <ashmitadangwal43@gmail.com>
2025-02-02 17:50:02 +0530

    Solution to Find and Display the Top Scorer

--- Find and Display the Top Scorer@@67877d61dc107486ad5c2744/code.c
+++ Find and Display the Top Scorer@@67877d61dc107486ad5c2744/code.c
@@ -16,7 +16,7 @@ int main()
         scanf("%d %s %f",&students[i].roll,students[i].name,&students[i].marks);
     }
     int topindex=0;
-    for(int i=0;i<n;i++)
+    for(int i=1;i<n;i++)
     {
         if(students[i].marks>students[topindex].marks)
         {
