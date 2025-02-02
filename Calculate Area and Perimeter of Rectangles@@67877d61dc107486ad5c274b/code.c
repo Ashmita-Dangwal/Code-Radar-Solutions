f2d71c43b373076922f261f29865bdaa5d244248
Ashmita-Dangwal <ashmitadangwal43@gmail.com>
2025-02-02 20:13:21 +0530

    Solution to Calculate Area and Perimeter of Rectangles

---
+++ Calculate Area and Perimeter of Rectangles@@67877d61dc107486ad5c274b/code.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include<string.h>
+struct Rectangle
+{
+    int l;
+    int b;
+};
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    struct Recatangle r1[n];
+    for(int i=0;i<n;i++)
+    {
+        sacnf("%d %d",&r1[i].l,&r1[i].b);
+    }
+    float a=l*b;
+    float p=2*(l+b);
+    for(int i=0;i<n;i++)
+    {
+        printf("Reactangle %d: Area = %.2f, Perimeter = %.2f",1,a,p);
+    }
+    return 0;
+}
