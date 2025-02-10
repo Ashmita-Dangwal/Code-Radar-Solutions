edea1be430a1d505ed549045db29b95fd5e15e7c
Ashmita-Dangwal <ashmitadangwal43@gmail.com>
2025-02-10 20:23:50 +0530

    Solution to Find the First Unique Character

---
+++ Find the First Unique Character@@67877c59ddaeab38c8ad25b4/code.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+char firstUniqueChar(char str[]) 
+{
+    int count[256] = {0};  
+    for (int i = 0; str[i] != '\0'; i++) 
+    {
+        count[(int)str[i]]++;
+    }
+    for (int i = 0; str[i] != '\0'; i++) 
+    {
+        if (count[(int)str[i]] == 1) 
+        {
+            return str[i]; 
+        }
+    }
+    return '\0';
+}
+int main() 
+{
+    char str[100];
+    fgets(str, sizeof(str), stdin);
+    str[strcspn(str, "\n")] = '\0';
+    char unique = firstUniqueChar(str);
+    if (unique)
+        printf("%c\n", unique);
+    else
+        printf("No unique character found.\n");
+    return 0;
+}
