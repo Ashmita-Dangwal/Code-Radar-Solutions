9193027fdbc813b2252604c7e842c03b204a2d6f
Ashmita Dangwal <ashmitadangwal43@gmail.com>
2025-03-09 10:10:13 +0530

    Solution to Caesar Cipher Encryption

---
+++ Caesar Cipher Encryption@@67ca7acd1fe08d7207c253c3/code.c
@@ -0,0 +1,10 @@
+void caesarCipher(char message[] , int shift , char encrypted[]){
+
+    int i=0;
+    while (message[i] !='\0'){
+        char l = message[i] + shift;
+        encrypted[i] = l;
+        i++;
+    }
+
+}
